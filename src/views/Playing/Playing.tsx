51f03d936425264819f4482a0883d3d094568c19
Oleksandr <oom2212@gmail.com>
2021-07-16 18:12:55 +0300

    add update Leardned

--- src/views/Playing/Playing.tsx
+++ src/views/Playing/Playing.tsx
@@ -1,15 +1,32 @@
-import React, {FC, ReactElement} from 'react';
-
+import React, {FC, ReactElement,useEffect} from 'react';
+import axios from "axios";
 import s from './Checking.module.scss';
 
-const Playing:FC=():ReactElement=> {
+const Playing: FC = (): ReactElement => {
 
 
-  return (
-    <div className={s.wrapper}>
-      <h2>Comming soon</h2>
-    </div>
-  );
+    useEffect(() => {
+
+        foo()
+
+    }, []);
+
+    const foo=async ()=>{
+        try {
+            const data = await axios.get('https://polaris.brighterir.com/public/glenveagh_properties/price_feed/json_price?key=rgydmrge');
+            console.log(data);
+        }
+        catch (err){
+            console.dir(err)
+        }
+    }
+
+    return (
+        <div className={s.wrapper}>
+            <h2>Comming soon</h2>
+
+        </div>
+    );
 }
 
 export default Playing;
